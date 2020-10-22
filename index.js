97af1f1cbaf9f6f0afe3cfe8f0d623f3b3de601a
André Almeida <andre_rdo@hotmail.com>
2020-10-22 01:29:43 -0300

    add get by id

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 const movies = require('./data.json')
+
 
 //GET
 app.get('/', (req, res) => {
+    res.send('<h1>This is the Vidly App</h1>')
+})
+
+app.get('/api/movies', (req, res) => {
     res.send(movies)
 })
 
+app.get('/api/movies/:id', (req, res) => {
+    const movie = movies.find(m => m.id === parseInt(req.params.id))
+    if (!movie) return res.status(404).send('<h1>Id not found</h1>')
+    res.send(movie)
+})
+
+
 //POST
 
+
+
 //PUT
+
 
 //DELETE
 
